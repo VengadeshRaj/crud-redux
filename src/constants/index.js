be6a1dd6113520134d5dc89ca1a0392bfd9abd53
vengadeshraj <vengadeshraj405@gmail.com>
2026-01-19 21:27:25 +0530

    few more dev added for form

---
+++ src/constants/index.js
@@ -0,0 +1,41 @@
+export const GENDERS = [
+  {
+    refCode: "",
+    value: "Select",
+  },
+  {
+    refCode: "M",
+    value: "Male",
+  },
+  {
+    refCode: "FM",
+    value: "Female",
+  },
+  {
+    refCode: "TG",
+    value: "Transgender",
+  },
+  {
+    refCode: "OT",
+    value: "Others",
+  },
+];
+
+export const DOMAINS = [
+  {
+    refCode: "D",
+    value: "Development",
+  },
+  {
+    refCode: "QA",
+    value: "Quality",
+  },
+  {
+    refCode: "B",
+    value: "Business",
+  },
+  {
+    refCode: "HR",
+    value: "Human Resource",
+  },
+];
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,4 +1,17 @@
+import { DOMAINS, GENDERS } from "../constants";
+
 export default function Form() {
+  const buildGender = () =>
+    GENDERS.map((g) => <option value={g.refCode}>{g.value}</option>);
+
+  const builDomain = () =>
+    DOMAINS.map((d) => (
+      <div className="flex flex-row gap-5">
+        <input type="radio" name="domain" className=""/>
+        <label className="font-medium">{d.value}</label>
+      </div>
+    ));
+
   return (
     <div className="flex flex-col justify-center h-screen items-center bg-gray-100 gap-10 p-5">
       <h1 className="text-3xl font-bold">Employee Registration Form</h1>
@@ -22,19 +35,22 @@ export default function Form() {
             {" "}
             Gender : <span className="text-red-500">*</span>
           </label>
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-2">
-              <input className="" name="gender" type="radio" />
-              <label className="font-medium">Male</label>
-            </div>
-            <div className="flex flex-row gap-2">
-              <input className="" name="gender" type="radio" />
-              <label className="font-medium">Female</label>
-            </div>
-            <div className="flex flex-row gap-2">
-              <input className="" name="gender" type="radio" />
-              <label className="font-medium">Others</label>
-            </div>
+          <div>
+            <select
+              className="w-64 border border-black size-8 rounded focus:outline-sky-600 "
+              defaultValue={""}
+            >
+              {buildGender()}
+            </select>
+          </div>
+        </div>
+        <div className="flex flex-row gap-10">
+          <label className="font-medium">
+            {" "}
+            Domain : <span className="text-red-500">*</span>
+          </label>
+          <div>
+            {builDomain()}
           </div>
         </div>
         <div className="flex flex-row gap-5">
